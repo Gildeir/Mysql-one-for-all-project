4d626341c9db2ef30397b53cf28dc090f63be1f7
Gildeir <gildeirlopes@gmail.com>
2021-08-05 01:34:06 -0300

    atualizando banco de dado e adicionando requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,11 @@
+USE SpotifyClone;
+CREATE VIEW top_2_hits_do_momento AS
+SELECT
+songs.song_name AS cancao,
+COUNT(historico_de_reproducoes.song_id) AS reproducoes
+FROM songs
+INNER JOIN historico_de_reproducoes
+ON historico_de_reproducoes.song_id = songs.song_id
+GROUP BY songs.song_name
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
