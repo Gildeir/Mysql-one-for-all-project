a1e8ade0c02a9bb2d393be9949eb6f6e914d783a
Gildeir <gildeirlopes@gmail.com>
2021-08-05 00:22:04 -0300

    adicionando req4

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+USE SpotifyClone;
+CREATE VIEW top_3_artistas AS
+SELECT
+artista.name,
+COUNT(seguindo_artistas.usuario_id) AS seguidores
+FROM artista
+INNER JOIN seguindo_artistas
+ON seguindo_artistas.artista_id = artista.artista_id
+GROUP BY artista.name
+ORDER BY seguidores DESC, artista.name
+LIMIT 3;
