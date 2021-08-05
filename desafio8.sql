e3288d076c5558b512dec2126ed40fa0eee31a11
Gildeir <gildeirlopes@gmail.com>
2021-08-05 06:10:00 -0300

    fixing 8

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,11 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.usuario
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.seguindo_artistas WHERE seguindo_artistas.usuario_id = old.usuario_id;
-DELETE FROM SpotifyClone.historico_de_reproducoes WHERE historico_de_reproducoes.usuario_id = old.usuario_id;
+DELETE FROM SpotifyClone.seguindo_artistas
+WHERE
+seguindo_artistas.usuario_id = old.usuario_id;
+DELETE FROM SpotifyClone.historico_de_reproducoes
+WHERE 
+historico_de_reproducoes.usuario_id = old.usuario_id;
 END $$
 DELIMITER ;
